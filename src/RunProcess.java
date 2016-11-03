e7a989c611528215557dfaf3000bdde97691f445
dprimenko <dprimenko@protonmail.com>
2016-11-03 14:31:06 +0100

    Initial commit

---
+++ src/RunProcess.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+
+public class RunProcess {
+	public static void main(String[] args) {
+		if (args.length <= 0) {
+			System.err.println("Debes dar un ejecutable");
+			System.exit(-1);
+		}
+		
+		ProcessBuilder pb = new ProcessBuilder(args);
+		
+		String pid;
+		pid = ManagementFactory.getRuntimeMXBean().getName();
+		System.out.println("El PID del padre es: " + pid);
+		
+		try {
+			Process proceso = pb.start();
+			int retorno = proceso.waitFor();
+			System.out.println("La ejecución del proceso " + Arrays.toString(args) + " devuelve " + retorno);
+			
+		} catch (IOException e) {
+			System.err.println("Error general de E/S");
+			System.exit(-1);
+		} catch (InterruptedException ex) {
+			System.err.println("El proceso hijo finalizó de forma inesperada");
+			System.exit(-1);
+		}
+	}
+
+}
